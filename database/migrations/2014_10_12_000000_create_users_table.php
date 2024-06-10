d46481c2ef4ae1e5d6a1b289106dc6bcf1d9bd26
Madan <140926459+MRamadhanSyahPutra@users.noreply.github.com>
2024-06-10 14:02:31 +0700

    2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('alamat');
-            $table->integer('no_telp');
+            $table->string('no_telp');
             $table->rememberToken();
             $table->timestamps();
         });
